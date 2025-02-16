f500b5daf436493fb016ecf82637d3bac298c2ba
vikitoriaSok <sme.vika@gmail.com>
2025-02-16 14:37:51 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -5,11 +5,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) 
-{
-	printf("Hello, Vika\n");     
+{	printf("Hello, Vika\n");     
 	printf("How old are you?\n");
-	return 0;
-}
+	return 0;}
+
+
 
 
 //ЗАДАЧА 2
@@ -17,16 +17,20 @@ int main(int argc, char *argv[])
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) 
-{
-	printf("Wake up, Neo...\n\n");
+{	printf("Wake up, Neo...\n\n");
 	printf("The Matrix has you...\n\n");
 	printf("Follow the white rabbit...\n\n");
+	return 0;}
 
 
-	return 0;
-}
 
 
-//ЗАДАЧА 3
+
+//ЗАДАЧА 2.1
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char *argv[]) 
+{	printf("Wake up, Neo...\n\nThe Matrix has you...\n\nFollow the white rabbit...\n\n");
+	return 0;}
