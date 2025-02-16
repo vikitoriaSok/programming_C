51cb716180ceee9db8cfa3713dfa82e60efecb8b
vikitoriaSok <sme.vika@gmail.com>
2025-02-16 14:34:48 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -11,3 +11,22 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+
+//ЗАДАЧА 2
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	printf("Wake up, Neo...\n\n");
+	printf("The Matrix has you...\n\n");
+	printf("Follow the white rabbit...\n\n");
+
+
+	return 0;
+}
+
+
+//ЗАДАЧА 3
