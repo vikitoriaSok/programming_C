537a5c4b34be33fbed9edd301725fd131292da72
Виктория) <sme.vika@gmail.com>
2025-02-17 15:52:54 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 #include <stdlib.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char *argv[]) 
-{	printf("Wake up, Neo...\n\nThe Matrix has you...\n\nFollow the white rabbit...\n\n");
+{	printf("Wake up, Neo...\n\nThe Matrix has you...\n\nFollow the white rabbit...\n");
 	return 0;}
 
 
