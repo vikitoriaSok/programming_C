514165aff10e069dcf90631e20c907305f0ac6a9
Виктория) <sme.vika@gmail.com>
2025-02-18 12:43:44 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -143,3 +143,18 @@ int main(int argc, char *argv[])
 	c=a+b;
 	printf( "a + b = %d",c);
 	return 0;}
+
+
+
+
+//ЗАДАЧА 8
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+{ 	int a = 16;
+	int b = 4;
+	int c;
+	c = a / b;
+	printf("%d",c);
+    return 0;}
