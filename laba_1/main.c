53af4e05b2af4269bdf2cb30b3c3617c15dc9b59
Виктория) <sme.vika@gmail.com>
2025-02-20 08:02:05 +0300

    Update main.c

--- laba_1/main.c
+++ laba_1/main.c
@@ -123,3 +123,64 @@ int main(int argc, char** argv[]) {
 
 
 //ЗАДАЧА 3. С ЦИКЛОМ. ДОМАШНЕЕ ЗАДАНИЕ
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+    char answer[10];
+    double a, b, c, d, x1, x2;
+    int i; // Для цикла приведения к нижнему регистру
+
+    // Основная часть программы
+    a = 0.5; // Значения по умолчанию
+    b = 1.2; // Значения по умолчанию
+    c = 3.4; // Значения по умолчанию
+
+    for (;;) {
+        printf("enter a:\n");
+        scanf("%lf", &a);
+        printf("ente b:\n");
+        scanf("%lf", &b);
+        printf("ente c:\n");
+        scanf("%lf", &c);
+
+        d = pow(b, 2) - 4 * a * c;
+        printf("d = %lf\n", d);
+
+        if (d > 0) {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            x2 = (-b - sqrt(d)) / (2 * a);
+            printf("x1 = %lf\n", x1);
+            printf("x2 = %lf\n", x2);
+        } else if (d < 0) {
+            printf("There are no roots\n");
+        } else {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            printf("x = %lf\n", x1);
+        }
+
+        // Запрос на продолжение
+        printf("continue the program yes or no: ");
+        scanf("%s", answer);
+
+        // Преобразование к нижнему регистру
+        for (i = 0; answer[i]; i++) {
+            answer[i] = tolower(answer[i]);
+        }
+
+        // Проверка ответа
+        if (strcmp(answer, "no") == 0) {
+            printf("The program is completed.\n");
+            break; // Выход из цикла for
+        } else if (strcmp(answer, "yes") == 0) {
+            printf("The program starts anew...\n");
+        } else {
+            printf("Incorrect input. Please enter 'yes' or 'no'.\n");
+        }
+    }
+
+    return 0;
+}
+
