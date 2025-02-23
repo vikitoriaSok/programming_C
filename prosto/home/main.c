f5a0c95bb137856c7f5913985a876c2d71e86d9e
Виктория) <sme.vika@gmail.com>
2025-02-23 16:16:35 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -442,6 +442,23 @@ int main(int argc, char** argv) {
 
 
 
+//ЗАДАЧА 22
+int main(void)
+{
+    for(int i=0; i < 5; i++)
+    {
+        printf("Hello Work\n");
+    }
+ 
+    int count = 0;
+    while(count < 5)
+    {
+        printf("Hello Work\n");
+        count++;
+    }
+     
+    return 0;
+}
 Цикл do..while
 //ЗАДАЧА 22
 	
