48fa1d11d459f50c48d12430284b20137db8f068
Виктория) <sme.vika@gmail.com>
2025-02-24 11:52:48 +0300

    Update main.c

--- laba_0/main.c
+++ laba_0/main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	printf("hello world");
-	return 0;
-}
+	return 0;}
