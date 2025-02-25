5a90879ae757eb81f44106397b7c39086d1f18c7
Виктория) <sme.vika@gmail.com>
2025-02-25 14:04:12 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -523,3 +523,25 @@ int main() {
 }
 
 
+//ПОВТОР
+#include <stdio.h>
+
+
+int main() {
+
+	int a;
+	int b;
+	int c;
+	
+	printf("enter a: ");
+	scanf("%d", &a);
+	
+	printf("enter b: ");
+	scanf("%d", &b);
+	
+	c = a + b;
+	
+	printf("a + b = %d", c);
+	
+  return 0;
+}
