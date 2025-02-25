25633ceb8f4f8212550f0762a821b6379a626750
Виктория) <sme.vika@gmail.com>
2025-02-25 12:39:20 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -459,6 +459,27 @@ int main(void)
      
     return 0;
 }
-Цикл do..while
+
 //ЗАДАЧА 22
-	
+#include <iostream>
+#include <stdio.h>
+
+
+int main(void)
+{
+	int x=2;
+	switch (x)
+	{
+		case 1:
+			printf("x=1\n");
+			break;
+		case 2:
+			printf("x=2\n");
+			break;
+		case 3: 
+            printf("x = 3 \n");
+            break;
+	}
+
+     
+    return 0;	
