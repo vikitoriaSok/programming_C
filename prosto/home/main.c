22806de5ec991d1ab145084299933adb89d49ba6
Виктория) <sme.vika@gmail.com>
2025-02-28 15:30:20 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -32,7 +32,6 @@ int main(int argc, char *argv[])
 //ЗАДАЧА 3
 #include <stdio.h>
 #include <stdlib.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char *argv[]) 
 {	printf("NICKNAME: NEO\n"
 	"city: Vladivostok\n"
