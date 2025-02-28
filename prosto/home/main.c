2f0fde948776d41c6d958179181ab32e49d159e6
Виктория) <sme.vika@gmail.com>
2025-02-28 15:29:55 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) 
 {	printf("Hello, Vika\n");     
 	printf("How old are you?\n");
