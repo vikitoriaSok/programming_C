9d505af98a7026ecfd649fa9487beaa8303e7e68
Виктория) <sme.vika@gmail.com>
2025-03-14 02:56:03 +0300

    Update main.c

--- prosto/home/main.c
+++ prosto/home/main.c
@@ -19,3 +19,28 @@ int main(int argc, char *argv[]) {
 	
 	return 0;
 }
+
+
+
+//ЗАДАЧА 2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Дана сторона квадрата a. Найти его площадь S = a2 */
+
+int main(int argc, char *argv[]) {
+	
+	int a;
+	int p;
+	
+	printf("Enter a: ");
+	scanf("%d", &a);
+	
+	p = 4 * a;
+	printf("Perimeter = %d", p);
+	
+	
+
+	return 0;
+}
