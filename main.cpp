81f8563395704d92b2938237a1e886938aa53808
Виктория) <sme.vika@gmail.com>
2025-03-16 09:20:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+	
+ char s[80];
+ int i, len;
+ 
+ printf("vvedite stroky do 80ti simvolov:\n");
+ scanf("%s", s);
+ 
+ len = strlen(s); 
+ 
+ for(i = 0; i < len; i++)
+ 
+ {
+ 	
+  if(s[i]== 'a')
+  {
+  s[i] = 'A';
+ }
+ 
+  if(s[i]=='b')
+  {  
+  s[i] = 'B';
+ }
+ 
+  if(s[i]=='.')
+  {  
+  s[i] = '!';
+ }
+ 
+ 
+ }
+ 
+ printf("%s", s); 
+ return 0;
+ 
+}
+
+
