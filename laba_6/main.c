86eabb0b9c3a59fcbd8191f770c260ac2b0e7ffe
Виктория) <sme.vika@gmail.com>
2025-03-29 11:10:53 +0300

    Update main.c

--- laba_6/main.c
+++ laba_6/main.c
@@ -1,3 +1,5 @@
+//https://metanit.com/c/tutorial/7.4.php?ysclid=m8toj4g5dz538518143 - пригодилось для работы ( прочитать файл, скопировать файл)
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,56 +16,8 @@
 /*char buffer[256]; - объявление буфера размером 256 символов
 FILE*fp=fopen(filename,"r"); - открытие файла в режиме чтения*/
 
-int main(int argc, char *argv[]) {
-	
-	//чтение текстового файла
-	char * filename1 = "text_name.txt";
-	char buffer[256];
-	FILE*fp=fopen(filename1,"r");
-	if(fp)
-	{
-		while((fgets(buffer, 256, fp))!=NULL)
-		{
-			printf("%s", buffer);
-		}
-		fclose(fp);
-	}
 
 
 
 
 
-
-
-
-
-
-	
-	//копирование списка
-	char *filename1 = "text_name";
-	char *filename2 = "text_pustoy";
-	char buffer[256];
-	FILE * f1 = fopen(filename1, "r");
-	FILE * f2 = fopen(filename2, "w");
-	if(!f1 || !f2)
-	{
-		printf("ERROR opening file\n");
-	}
-	else:
-		{
-			while((fgets(buffer,256,f1))!=NULL)
-			{
-				fputs(buffer, f2);
-				printf("%s", buffer);
-			}
-		}
-	
-	
-	
-	
-	
-	fclose(f1);
-	fclose(f2);
-	
-	return 0;
-}
